f11cc9f525e40c74d0742b99eaf00859aa323bf3
jinna <76821561+jinna124@users.noreply.github.com>
2024-08-15 11:13:44 -0400

    Update emotion_detection.py with errror handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,13 +8,21 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json = myobj, headers=header) 
     formatted_response = json.loads(response.text)
     emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
-    anger_score = emotion_scores['anger']
-    disgust_score = emotion_scores['disgust']
-    fear_score = emotion_scores['fear']
-    joy_score = emotion_scores['joy']
-    sadness_score = emotion_scores['sadness']
-
-    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    
+    if response.status_code == 200:
+        anger_score = emotion_scores['anger']
+        disgust_score = emotion_scores['disgust']
+        fear_score = emotion_scores['fear']
+        joy_score = emotion_scores['joy']
+        sadness_score = emotion_scores['sadness']
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    elif response.status_code == 400: 
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
     
     return {
     'anger': anger_score,
